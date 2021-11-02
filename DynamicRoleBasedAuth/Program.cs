d7728ef052be8e5e735c6c7352ed291af1bfcc58
payambordbar <45229646+payambordbar@users.noreply.github.com>
2021-11-02 17:00:32 +0330

    Add initial migration and seedData

--- DynamicRoleBasedAuth/Program.cs
+++ DynamicRoleBasedAuth/Program.cs
@@ -1,11 +1,17 @@
+using DynamicRoleBasedAuth.Data;
+using DynamicRoleBasedAuth.Identity;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using DynamicRoleBasedAuth.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+    options.UseSqlServer(connectionString);
+    options.LogTo(Console.WriteLine);
+});
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
  {
@@ -23,7 +29,11 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddSingleton<ActionDetection>();
+
 var app = builder.Build();
+
+await SeedDatabase(app.Services);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -55,3 +65,36 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
+static async Task SeedDatabase(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var adminUser = new IdentityUser()
+    {
+        UserName = "admin@site.com",
+        Email = "admin@site.com",
+        EmailConfirmed = true
+    };
+    var adminRole = new IdentityRole(Roles.Admin);
+    var userRole = new IdentityRole(Roles.User);
+
+    if ((await roleManager.FindByNameAsync(Roles.Admin)) is null)
+    {
+        await roleManager.CreateAsync(adminRole);
+    }
+
+    if ((await roleManager.FindByNameAsync(Roles.User)) is null)
+    {
+        await roleManager.CreateAsync(userRole);
+    }
+
+    if ((await userManager.FindByEmailAsync(adminUser.Email)) is null)
+    {
+        await userManager.CreateAsync(adminUser, "pass");
+        await userManager.AddToRoleAsync(adminUser, Roles.Admin);
+    }
+}
