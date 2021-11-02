f8a93b563fea0cb3c39e6b710cd88f41f5ad2ec0
payambordbar <45229646+payambordbar@users.noreply.github.com>
2021-11-02 16:55:16 +0330

    Add Identity Index page

---
+++ DynamicRoleBasedAuth/Areas/Identity/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DynamicRoleBasedAuth.Areas.Identity.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
